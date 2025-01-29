02d1d3efd4543e73a35eeb7904675b1b3dafee1a
PujaGupta2786 <66173785+PujaGupta2786@users.noreply.github.com>
2025-01-29 13:56:00 +0530

    Day1 & day2

---
+++ 02-Day2-Application-of-Python/5. Snake Game/Snake.py
@@ -0,0 +1,195 @@
+author = 'Puja'
+
+# Importing The Modules
+import pygame
+import random
+import os
+import sys
+
+# Initialization
+pygame.mixer.init()
+pygame.init()
+
+# Colors
+white = (255, 255, 255)
+red = (255, 0, 0)
+black = (0, 0, 0)
+snakegreen = (35, 45, 40)
+
+# Game Backgrounds
+try:
+    bg2 = pygame.image.load("Screen/bg2.jpg")
+    intro = pygame.image.load("Screen/intro1.png")
+    outro = pygame.image.load("Screen/outro.png")
+except pygame.error as e:
+    print(f"Unable to load image: {e}")
+    pygame.quit()
+    sys.exit()
+
+# Creating The window
+screen_width = 800
+screen_height = 600
+gameWindow = pygame.display.set_mode((screen_width, screen_height))
+
+# Game Title
+pygame.display.set_caption("Snake By Puja")
+pygame.display.update()
+
+# Music
+try:
+    pygame.mixer.music.load('music/wc.mp3')
+    pygame.mixer.music.play(100)
+    pygame.mixer.music.set_volume(.6)
+except pygame.error as e:
+    print(f"Unable to load music: {e}")
+    pygame.quit()
+    sys.exit()
+
+# Variables For The Game
+clock = pygame.time.Clock()
+font = pygame.font.SysFont('Harrington', 35)
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x, y])
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x, y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+# Welcome Screen
+def welcome():
+    exit_game = False
+    while not exit_game:
+        gameWindow.blit(intro, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    pygame.mixer.music.fadeout(200)
+                    try:
+                        pygame.mixer.music.load('music/bgm.mp3')
+                        pygame.mixer.music.play(100)
+                        pygame.mixer.music.set_volume(.6)
+                    except pygame.error as e:
+                        print(f"Unable to load music: {e}")
+                        pygame.quit()
+                        sys.exit()
+                    gameloop()
+        pygame.display.update()
+        clock.tick(60)
+
+# Game Loop
+def gameloop():
+    # Game specific variables
+    exit_game = False
+    game_over = False
+    snake_x = 45
+    snake_y = 55
+    velocity_x = 0
+    velocity_y = 0
+    snk_list = []
+    snk_length = 1
+
+    # Highscore Build
+    if not os.path.exists("highscore.txt"):
+        with open("highscore.txt", "w") as f:
+            f.write("0")
+    with open("highscore.txt", "r") as f:
+        highscore = f.read()
+
+    # Food
+    food_x = random.randint(20, screen_width - 20)
+    food_y = random.randint(20, screen_height - 20)
+
+    # Game Variables
+    score = 0
+    init_velocity = 5
+    snake_size = 30
+    fps = 60
+
+    while not exit_game:
+        if game_over:
+            with open("highscore.txt", "w") as f:
+                f.write(str(highscore))
+
+            # GameOverScreen
+            gameWindow.blit(outro, (0, 0))
+            text_screen("Score: " + str(score), snakegreen, 385, 350)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_game = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        welcome()
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_game = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        velocity_x = init_velocity
+                        velocity_y = 0
+                    if event.key == pygame.K_LEFT:
+                        velocity_x = -init_velocity
+                        velocity_y = 0
+                    if event.key == pygame.K_UP:
+                        velocity_y = -init_velocity
+                        velocity_x = 0
+                    if event.key == pygame.K_DOWN:
+                        velocity_y = init_velocity
+                        velocity_x = 0
+                    if event.key == pygame.K_q:
+                        score += 10
+
+            snake_x += velocity_x
+            snake_y += velocity_y
+
+            if abs(snake_x - food_x) < 20 and abs(snake_y - food_y) < 20:
+                score += 10
+                food_x = random.randint(20, screen_width - 20)
+                food_y = random.randint(20, screen_height - 20)
+                snk_length += 5
+                if score > int(highscore):
+                    highscore = score
+
+            gameWindow.blit(bg2, (0, 0))
+            text_screen("Score: " + str(score) + "  Highscore: " + str(highscore), snakegreen, 5, 5)
+            pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
+            head = [snake_x, snake_y]
+            snk_list.append(head)
+
+            if len(snk_list) > snk_length:
+                del snk_list[0]
+
+            if head in snk_list[:-1]:
+                game_over = True
+                try:
+                    pygame.mixer.music.load('music/bgm1.mp3')
+                    pygame.mixer.music.play(100)
+                    pygame.mixer.music.set_volume(.6)
+                except pygame.error as e:
+                    print(f"Unable to load music: {e}")
+                    pygame.quit()
+                    sys.exit()
+
+            if snake_x < 0 or snake_x > screen_width or snake_y < 0 or snake_y > screen_height:
+                game_over = True
+                try:
+                    pygame.mixer.music.load('music/bgm2.mp3')
+                    pygame.mixer.music.play(100)
+                    pygame.mixer.music.set_volume(.6)
+                except pygame.error as e:
+                    print(f"Unable to load music: {e}")
+                    pygame.quit()
+                    sys.exit()
+
+            plot_snake(gameWindow, black, snk_list, snake_size)
+        pygame.display.update()
+        clock.tick(fps)
+
+    pygame.quit()
+    quit()
+
+welcome()
